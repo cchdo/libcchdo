f6e9537862972a4279f49830ff54444382dbc924
Matthew Shen <myshen@cchdo.ucsd.edu>
2009-09-02 16:08:59 -0700

    py: Make wrapper errors for cchdo_to_oceansites_netcdf more descriptive.

--- bin/cchdo_to_oceansites_netcdf.py
+++ bin/cchdo_to_oceansites_netcdf.py
@@ -4,8 +4,8 @@ from __future__ import with_statement
 import libcchdo
 
 from sys import argv
-if len(argv) < 2:
-  print 'Need argument'
+if len(argv) < 3:
+  print 'Usage:', argv[0], '<cchdo_netcdf> <oceansites_destination>'
   exit(1)
 file = libcchdo.DataFile()
 with open(argv[1], 'r') as in_file:
