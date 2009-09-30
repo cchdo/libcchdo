7d7ae83028b501c0582c2db4db80f104e239e926
Matt Shen <synmantics@gmail.com>
2009-09-30 14:32:37 -0700

    Add clean command to setup.py and credit

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from glob import glob
 from os.path import splitext, basename, join as pjoin, walk
 import os
 
+# http://da44en.wordpress.com/2002/11/22/using-distutils/
 class TestCommand(Command):
   description = "Runs tests"
   user_options = []
@@ -23,9 +24,30 @@ class TestCommand(Command):
     tests = TestLoader().loadTestsFromNames(testfiles)
     TextTestRunner(verbosity = 2).run(tests)
 
+class CleanCommand(Command):
+  description = "Cleans directories of .pyc files"
+  user_options = []
+
+  def initialize_options(self):
+    self._clean_me = [ ]
+    for root, dirs, files in os.walk('.'):
+      for f in files:
+        if f.endswith('.pyc'):
+          self._clean_me.append(pjoin(root, f))
+  def finalize_options(self):
+    pass
+  def run(self):
+    for clean_me in self._clean_me:
+      try:
+        os.unlink(clean_me)
+      except:
+        pass
+
 setup(name="libcchdo",
       version="0.4",
       description="libcchdo setup",
-      cmdclass = {'test': TestCommand}
+      cmdclass = {'test': TestCommand,
+                  'clean': CleanCommand
+                 }
      )
 
