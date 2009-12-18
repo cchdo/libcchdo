228e62801068ac9cf5627bf6e5767d34af367452
Matt Shen <synmantics@gmail.com>
2009-12-17 16:46:51 -0800

    Finish initial coding of nav_to_kml in bin

--- bin/nav_to_kml.py
+++ bin/nav_to_kml.py
@@ -1,40 +1,57 @@
 #!/usr/bin/env python
-# nav_to_kml
-
-
-#TODO TODO TODO
 
 from __future__ import with_statement
+
+from datetime import datetime
+from string import translate, maketrans
+from os import path, makedirs, getcwd
+
+from sys import argv, exit, stdout
+import sys
+sys.path.insert(0, '/'.join(sys.path[0].split('/')[:-1]))
+import libcchdo
+from datadir.util import do_for_cruise_directories
 
 def color_arr_to_str(color):
   return 'ff'+''.join(map(lambda x: '%02x' % x, color[::-1]))
 
 kml_header = """<?xml version="1.0" encoding="UTF-8"?>
-<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom"><Document>"""
+<kml xmlns="http://www.opengis.net/kml/2.2"
+     xmlns:gx="http://www.google.com/kml/ext/2.2"
+     xmlns:kml="http://www.opengis.net/kml/2.2"
+     xmlns:atom="http://www.w3.org/2005/Atom"><Document>"""
 kml_footer = """</Document></kml>"""
 
-directory = './KML_CCHDO_holdings_'+translate(str(datetime.utcnow()), maketrans(' :.', '___'))
+cwd=getcwd()
+directory = cwd+'/KML_CCHDO_holdings_'+translate(str(datetime.utcnow()), maketrans(' :.', '___'))
 if not path.exists(directory):
   makedirs(directory)
 
 cycle_colors = map(color_arr_to_str, [[255, 0, 0], [0, 255, 0], [0, 0, 255], [255, 255, 0]])
 
-connection = connect_mysql()
-cursor = connection.cursor()
-cursor.execute('SELECT ExpoCode,ASTEXT(track) FROM track_lines')
-rows = cursor.fetchall()
-for i, row in enumerate(rows):
-  expocode = row[0]
+def generate_kml_from_nav(root, dirs, files, outputdir):
+  try:
+    navfile = filter(lambda x: x.endswith('na.txt'), files)[0]
+  except:
+    navfile = None
+  if not 'ExpoCode' in files or not navfile:
+    print 'Skipping KML generation for %s. Not enough info found.' % root
+    return False
+  print 'Attempting generation for %s.' % root
+  with open(path.join(root, 'ExpoCode'), 'r') as f:
+    expocode = f.read()[:-1]
+  # use nav file to gen kml
+  with open(path.join(root, navfile), 'r') as f:
+    coords = map(lambda l: l.split(), f.readlines())
+  if not coords: return False
   placemarks = []
-  coordstr = translate(row[1][11:], maketrans(', ', ' ,'))
-  coords = map(lambda x: x.split(','), coordstr.split(' '))
   placemarks.append("""
 <Style id="linestyle">
   <LineStyle>
     <width>4</width>
     <color>%s</color>
   </LineStyle>
-</Style>""" % cycle_colors[i%len(cycle_colors)])
+</Style>""" % cycle_colors[generate_kml_from_nav.i%len(cycle_colors)])
   placemarks.append("""
 <Placemark>
   <name>%s</name>
@@ -43,7 +60,7 @@ for i, row in enumerate(rows):
     <tessellate>1</tessellate>
     <coordinates>%s</coordinates>
   </LineString>
-</Placemark>""" % (expocode, coordstr))
+</Placemark>""" % (expocode, ' '.join(map(lambda c: '%s,%s' % (c[0], c[1]), coords))))
   placemarks.append("""
 <Placemark>
   <styleUrl>#start</styleUrl>
@@ -51,14 +68,17 @@ for i, row in enumerate(rows):
   <description>http://cchdo.ucsd.edu/data_access/show_cruise?ExpoCode=%s</description>
   <Point><coordinates>%s,%s</coordinates></Point>
 </Placemark>""" % (expocode, expocode, coords[0][0], coords[0][1]))
+  placemarks.append('<Folder>')
   for coord in coords:
     placemarks.append("""
 <Placemark>
   <styleUrl>#pt</styleUrl>
+  <description>%s,%s</description>
   <Point><coordinates>%s,%s</coordinates></Point>
-</Placemark>""" % (coord[0], coord[1]))
+</Placemark>""" % (coord[0], coord[1], coord[0], coord[1]))
+  placemarks.append('</Folder>')
 
-  with open(directory+'/track_'+expocode+'.kml', 'w') as f:
+  with open(outputdir+'/track_'+expocode+'.kml', 'w') as f:
     f.write("""%s<name>%s</name>
 <Style id="start">
   <IconStyle>
@@ -78,6 +98,9 @@ for i, row in enumerate(rows):
   </IconStyle>
 </Style>
 %s%s""" % (kml_header, expocode, ''.join(placemarks), kml_footer))
-
-cursor.close()
-connection.close()
+  print 'Generated KML for %s.' % root
+  generate_kml_from_nav.i += 1
+generate_kml_from_nav.i = 0
+def generate_kml_from_nav_into(dir):
+  return lambda root, dirs, files: generate_kml_from_nav(root, dirs, files, dir)
+do_for_cruise_directories(generate_kml_from_nav_into(directory))
