cdf6ae0681eff0403c83bc57d5838f4cb1be1243
Matt Shen <myshen@ucsd.edu>
2010-07-14 09:36:34 -0700

    Add test framework for formats.ctd.woce

---
+++ tests/test_formats_ctd_woce.py
@@ -0,0 +1,26 @@
+""" Test case for libcchdo.formats.ctd.woce """
+
+import StringIO
+import sys
+import os
+from unittest import TestCase
+
+import libcchdo
+import libcchdo.formats.ctd.woce as ctdw
+
+class TestCTDWOCE(TestCase):
+
+  def setUp(self):
+      pass
+
+  def test_invalid_record1(self):
+  # TODO
+      pass
+
+  def test_invalid_record2(self):
+  # TODO
+      pass
+
+  def test_invalid_record3(self):
+  # TODO
+      pass
