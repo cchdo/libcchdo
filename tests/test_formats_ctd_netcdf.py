248e3ed32f4699546071b4e9bfa24964f4aecb45
Andrew Shen <ayshen@ucsd.edu>
2010-10-05 11:18:43 -0700

    Initial COARDS NetCDF CTD write(); Add test file for NetCDF CTD

---
+++ tests/test_formats_ctd_netcdf.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import unittest
+
+
+import libcchdo
+import libcchdo.formats.ctd.netcdf as ctdnc
+
+
+class TestCTDNetCDF (unittest.TestCase):
+
+    def setUp (self):
+        pass
+
+    def tearDown (self):
+        pass
+
+    def test_read (self):
+        self.assertTrue(True)
+
+    def test_write (self):
+        self.assertTrue(True)
+
