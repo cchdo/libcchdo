bf636013fb0d47e552f19fb9f9ae9361ca8a1b81
Matt Shen <myshen@ucsd.edu>
2011-04-20 12:15:32 -0700

    Change depth algorithms to use Decimal

--- libcchdo/fns.py
+++ libcchdo/fns.py
@@ -1,6 +1,7 @@
 """Functions used globally."""
 
 
+import decimal
 import math
 import os.path
 
@@ -195,3 +196,86 @@ def polynomial(x, coeffs):
         sum += coef * degreed
         degreed *= x
     return sum
+
+
+# The following are adapted from 
+# http://docs.python.org/library/decimal.html#decimal-recipes
+
+
+class IncreasedPrecision:
+
+    def __enter__(self):
+        decimal.getcontext().prec += 2
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        decimal.getcontext().prec -= 2
+
+
+def exp(x):
+    """Return e raised to the power of x.  Result type matches input type.
+
+    >>> print exp(Decimal(1))
+    2.718281828459045235360287471
+    >>> print exp(Decimal(2))
+    7.389056098930650227230427461
+    >>> print exp(2.0)
+    7.38905609893
+    >>> print exp(2+0j)
+    (7.38905609893+0j)
+
+    """
+    with IncreasedPrecision():
+        i, lasts, s, fact, num = 0, 0, 1, 1, 1
+        while s != lasts:
+            lasts = s
+            i += 1
+            fact *= i
+            num *= x
+            s += num / fact
+    return +s
+
+
+def cos(x):
+    """Return the cosine of x as measured in radians.
+
+    >>> print cos(Decimal('0.5'))
+    0.8775825618903727161162815826
+    >>> print cos(0.5)
+    0.87758256189
+    >>> print cos(0.5+0j)
+    (0.87758256189+0j)
+
+    """
+    with IncreasedPrecision():
+        i, lasts, s, fact, num, sign = 0, 0, 1, 1, 1, 1
+        while s != lasts:
+            lasts = s
+            i += 2
+            fact *= i * (i-1)
+            num *= x * x
+            sign *= -1
+            s += num / fact * sign
+    return +s
+
+
+def sin(x):
+    """Return the sine of x as measured in radians.
+
+    >>> print sin(Decimal('0.5'))
+    0.4794255386042030002732879352
+    >>> print sin(0.5)
+    0.479425538604
+    >>> print sin(0.5+0j)
+    (0.479425538604+0j)
+
+    """
+    with IncreasedPrecision():
+        i, lasts, s, fact, num, sign = 1, 0, x, 1, x, 1
+        while s != lasts:
+            lasts = s
+            i += 2
+            fact *= i * (i-1)
+            num *= x * x
+            sign *= -1
+            s += num / fact * sign
+    return +s
