591242ab37b7fb183693aaedf2b401e15d394cc7
Matt Shen <myshen@ucsd.edu>
2013-03-20 14:46:45 -0700

    Fix nav generation bug when gathering NavCoord info

--- libcchdo/model/navcoord.py
+++ libcchdo/model/navcoord.py
@@ -102,21 +102,37 @@ def iter_coords(self, navcoordcls, callback):
     if (isinstance(self, DataFile) or isinstance(self, SummaryFile)):
         # TODO it is possible for a datafile to have a global expocode...
         expocodes = self.expocodes()
+
+        lons = self['LONGITUDE'].values
+        lats = self['LATITUDE'].values
+        stnnbrs = self['STNNBR'].values
+        dates = self['_DATETIME'].values
+        try:
+            codes = self['_CODE'].values
+        except KeyError:
+            codes = [None] * len(lons)
+
         for expocode in expocodes:
-            indices = [i for i, x in enumerate(expocodes) if x == expocode]
-            lons = [self['LONGITUDE'][i] for i in indices]
-            lats = [self['LATITUDE'][i] for i in indices]
-            stnnbrs = [self['STNNBR'][i] for i in indices]
-            dates = [self['_DATETIME'][i] for i in indices]
+            elons = []
+            elats = []
+            estnnbrs = []
+            edates = []
+            ecodes = []
+
+            for i, x in enumerate(self['EXPOCODE'].values):
+                if x != expocode:
+                    continue
+                elons.append(lons[i])
+                elats.append(lats[i])
+                estnnbrs.append(stnnbrs[i])
+                edates.append(dates[i])
+                ecodes.append(codes[i])
+
+            items = zip(elons, elats, estnnbrs, edates, ecodes)
             try:
-                codes = [self['_CODE'][i] for i in indices]
+                expocode_coords[expocode].extend(items)
             except KeyError:
-                codes = [None] * len(lons)
-            items = zip(lons, lats, stnnbrs, dates, codes)
-            try:
-                expocode_coords[expocode].append(items)
-            except KeyError:
-                expocode_coords[expocode] = [items]
+                expocode_coords[expocode] = items
     elif isinstance(self, DataFileCollection):
         coords = [
             [file.globals['LONGITUDE'],
